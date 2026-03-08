230d6c97cef5ec35db6944a03873bf99ce807e24
Zen5168 <jexkeanalfonso@gmail.com>
2026-03-08 16:09:17 +0800

    Added student grades

--- src/a3/A3.java
+++ src/a3/A3.java
@@ -4,18 +4,29 @@ public class A3 {
 
     public static void main(String[] args) {
          String[] names = {
-             "Russel Arcega", 
+            "Russel Arcega", 
             "Dwight Dejy Del Coro", 
             "Christian Dolotallas", 
             "Joseph Angelo Dreu", 
             "Enzo Elijah Esteban", 
             "Seanjay Macabales",
-            "William Chester Pacaña",
+            "William Chester Pacana",
             "George Paul Padil",
             "John Mark Ezekiel Talotalo",
             "Kyle Zedrick Villamor"
         };
          
-         System.out.println("Testing");
+         double[][] grades = {
+             {89,90,92}, // Arcega
+             {90,94,97}, // Del Coro
+             {94,90,95}, // Dolotallas
+             {91,90,92}, // Dreu
+             {88,90,92}, //Esteban
+             {90,90,92}, // Macabales
+             {90,90,92}, // Pacana
+             {91,90,92}, // Padil
+             {90,95,94}, // Talotalo
+             {90,95,95}, // Villamor
+         };
     }
   }
