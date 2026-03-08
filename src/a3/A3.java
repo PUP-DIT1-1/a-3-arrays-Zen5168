772fa1f0fbb357e401c690cec40a259905c1fd2e
Zen5168 <jexkeanalfonso@gmail.com>
2026-03-08 15:56:05 +0800

    Starting my project

---
+++ src/a3/A3.java
@@ -0,0 +1,19 @@
+package a3;
+
+public class A3 {
+
+    public static void main(String[] args) {
+         String[] names = {
+             "Russel Arcega", 
+            "Dwight Dejy Del Coro", 
+            "Christian Dolotallas", 
+            "Joseph Angelo Dreu", 
+            "Enzo Elijah Esteban", 
+            "Seanjay Macabales",
+            "William Chester Pacaña",
+            "George Paul Padil",
+            "John Mark Ezekiel Talotalo",
+            "Kyle Zedrick Villamor"
+        };
+    }
+  }
